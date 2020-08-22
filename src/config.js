2479bf81f8821c9269b5aca162106094d8eb1934
Nathaniel Mata <nathaniel@thepixelparty.com>
2020-08-22 13:10:22 -0700

    use jwt secret from env

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DB_URL || 'postgresql://thingful@localhost/thingful',
-  TEST_DB_URL: process.env.TEST_DB_URL || "postgresql://thingful@localhost/thingful-test"
+  TEST_DB_URL: process.env.TEST_DB_URL || "postgresql://thingful@localhost/thingful-test",
+  JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
 }
